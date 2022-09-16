f8442726e6e3e938cf637eb08cc48eb87d0bfeec
liujun <522903052@qq.com>
2022-09-16 09:28:40 +0800

    建立user表

--- user_srv/model/models.py
+++ user_srv/model/models.py
@@ -1,8 +1,17 @@
+from tabnanny import verbose
 from sqlalchemy import Column
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Date, Text
 from db import Base
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30))
+    id = Column(Integer, primary_key=True, index=True)
+    mobile = Column(String(11), unique=True)
+    password = Column(String(100))
+    nick_name = Column(String(20), nullable=True)
+    head_url = Column(String(200), nullable=True)
+    birthday = Column(Date, nullable=True)
+    address = Column(String(200), nullable=True)
+    desc = Column(Text, nullable=True)
+    gender = Column(String, nullable=True)
+    role = Column(Integer)
