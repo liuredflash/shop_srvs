4703ab6c9c320f0bc983158d2069416378a0286f
liujun <522903052@qq.com>
2022-09-16 09:55:40 +0800

    修正user

--- user_srv/model/models.py
+++ user_srv/model/models.py
@@ -6,12 +6,12 @@ from db import Base
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, index=True)
-    mobile = Column(String(11), unique=True)
+    mobile = Column(String(11), unique=True, nullable=False)
     password = Column(String(100))
     nick_name = Column(String(20), nullable=True)
     head_url = Column(String(200), nullable=True)
     birthday = Column(Date, nullable=True)
     address = Column(String(200), nullable=True)
     desc = Column(Text, nullable=True)
-    gender = Column(String, nullable=True)
+    gender = Column(String(20), nullable=True)
     role = Column(Integer)
